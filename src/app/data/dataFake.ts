5178ea2b27afab6fb7ca48ea559b1ecfd4355496
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-10-10 23:16:59 -0300

    pages - content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,25 +1,29 @@
 export const dataFake = [
   {
-    id: 1,
-    photo: 'dfdf',
+    id: '1',
+    photo:
+      'https://media.istockphoto.com/id/1476264587/pt/foto/young-man-using-a-digital-tablet-in-a-city-at-night.jpg?s=2048x2048&w=is&k=20&c=OYAl9Kq70JRJfHEbyDKTD9kV6wsd8HOVIazXKnu6Tbc=',
     title: 'Titulo para teste do ID 1',
     description: 'descrição para teste do ID 1',
   },
   {
-    id: 2,
-    photo: 'dfdf',
+    id: '2',
+    photo:
+      'https://media.istockphoto.com/id/1436767563/pt/foto/happy-it-technician-working-at-the-office-using-her-laptop.jpg?s=2048x2048&w=is&k=20&c=WnCht2s4sxdF__W1mhkzA9GIp1mEnZWlrmaowRwGYK8=',
     title: 'Titulo para teste do ID 2',
     description: 'descrição para teste do ID 2',
   },
   {
-    id: 3,
-    photo: 'dfdf',
+    id: '3',
+    photo:
+      'https://media.istockphoto.com/id/1435220822/pt/foto/african-american-software-developer.jpg?s=2048x2048&w=is&k=20&c=M_MJ1BvZw6-vA7FVRqFHspAi9CQwzLiHE81VXjB71Qs=',
     title: 'Titulo para teste do ID 3',
     description: 'descrição para teste do ID 3',
   },
   {
-    id: 4,
-    photo: 'dfdf',
+    id: '4',
+    photo:
+      'https://images.unsplash.com/photo-1529071753386-dfb44ff59f69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
     title: 'Titulo para teste do ID 4',
     description: 'descrição para teste do ID 4',
   },
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,13 +8,22 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  contentPhotoCover: string =
-    'https://media.istockphoto.com/id/1476264587/pt/foto/young-man-using-a-digital-tablet-in-a-city-at-night.jpg?s=2048x2048&w=is&k=20&c=OYAl9Kq70JRJfHEbyDKTD9kV6wsd8HOVIazXKnu6Tbc=';
+  contentPhotoCover: string = '';
   contentTitle: string = '';
   contentDescription: string = '';
+  private id: string | null = '0';
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((value) => console.log(value));
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+    this.setValuesToComponents(this.id);
+  }
+  setValuesToComponents(id: string | null) {
+    const result = dataFake.filter((article) => article.id == id)[0];
+
+    this.contentDescription = result.description;
+    this.contentPhotoCover = result.photo;
+    this.contentTitle = result.title;
   }
 }
